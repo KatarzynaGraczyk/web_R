1b6f74305437e2c85a83d40db5ac9ee0f1dd5af7
kgruba <kgruba92@gmail.com>
2017-03-11 19:27:17 +0100

    Rearanged ui

--- server.R
+++ server.R
@@ -6,8 +6,9 @@ library(data.table)
 
 shinyServer(function(input, output, session) {
 
-#------------- Table reading ------------------
-  values <- reactiveValues(df_data = NULL)
+#---------------------- Table reading ---------------------------
+  
+  values <- reactiveValues(df_data = NULL, sel.table = NULL)
   
   observeEvent(input$read, {
     progress <- Progress$new(session, min = 1, max = 15)
@@ -15,25 +16,38 @@ shinyServer(function(input, output, session) {
     progress$set(message = 'Reading table in progress ...')
     values$df_data <- fread(input$file$datapath,  h = T)
   })
-
-  output$table <- renderDataTable({values$df_data})
   
+  generate.checkbox <- reactive({
+    validate(
+      need(input$read, "Please read file with your data")
+    )
+  })
+  
+  output$factorcheckboxes <- renderUI({
+    generate.checkbox()
+    factornames <- colnames(values$df_data)
+    checkboxGroupInput(inputId = "variable",label = "Variables:", choices = factornames, selected = "all", inline = FALSE)
+  })
+  
+  output$table = renderDataTable({
+    values$df_data
+  })
 # ------------ UI rendering - update select input ----------------
+
   observe({  
-  updateSelectInput(session, "variable.x", choices = colnames(values$df_data))
-  updateSelectInput(session, "variable.y", choices = colnames(values$df_data))
-  updateSelectInput(session, "variable.color" ,choices = c("NULL",colnames(values$df_data)))
+    updateSelectInput(session, "variable.x", choices = colnames(values$df_data))
+    updateSelectInput(session, "variable.y",  choices = colnames(values$df_data))
+    updateSelectInput(session, "variable.color", choices = c("NULL",colnames(values$df_data)))
   })
 
 #------------------------- plots ---------------------------------
   plot.dat <- reactiveValues(main = NULL, layer = NULL)
   
-   
   observeEvent(input$plottype, {
-    plot.dat$main <- ggplot(data=values$df_data, aes(x = values$df_data[[input$variable.x]], 
+    plot.dat$main <- ggplot(data = values$df_data, aes(x = values$df_data[[input$variable.x]], 
                                                      y = values$df_data[[input$variable.y]])) + 
       theme(axis.text.x = element_text(angle = 90, hjust = 0.5), plot.title = element_text(hjust = 0.5)) +
-      labs(list(title = paste(input$variable.x, "vs" ,input$variable.y, sep = " "), x = input$variable.x, y = input$variable.y))
+      labs(list(title = paste(input$variable.x, "vs" ,input$variable.y, sep = " "), x = input$variable.x, y = input$variable.y, color = "Group by:\n"))
     if (input$plottype == "box") {
       observeEvent(input$variable.color, {if (input$variable.color == "NULL") {
         plot.dat$layer <- geom_boxplot()
@@ -45,7 +59,7 @@ shinyServer(function(input, output, session) {
        observeEvent(input$variable.color, {if (input$variable.color == "NULL") {
          plot.dat$layer <- geom_bar(stat = "identity")
        } else {
-         plot.dat$layer <- geom_bar(mapping = aes(colour = values$df_data[[input$variable.color]]), stat = "identity")
+         plot.dat$layer <- geom_bar(mapping = aes(fill = values$df_data[[input$variable.color]]), stat = "identity", position = "dodge")
        }})
     } else if (input$plottype == "scat") {
       observeEvent(input$variable.color, {if (input$variable.color == "NULL") {
--- ui.R
+++ ui.R
@@ -1,41 +1,53 @@
 library(shinythemes)
 
-
-
 shinyUI(
-   navbarPage("My first shiny app",
-                    tabPanel("Plots and tables", 
+   navbarPage("Data viewer",
+                    tabPanel("Tables", 
                              fluidPage(theme = shinytheme("united"),
-                               sidebarLayout(
-                                 sidebarPanel(fileInput( "file", label = "Input your data" ),
-                                        actionButton( "read", label = "Read table" ),
-                                        br(),
-                                        hr(),
-                                        selectInput(inputId = "variable.x",label = "X axis:", choices = "Read table first", selected = "all"),
-                                        selectInput(inputId = "variable.y",label = "Y axis:", choices = "Read table first", selected = "all"),
-                                        selectInput(inputId = "variable.color",label = "Group by:",  choices = "Read table first", selected = "all"),
-                                        width = 3),
-                                 mainPanel(
-                                   tabsetPanel(
-                                     tabPanel("Table", br(), dataTableOutput( outputId = "table")),
-                                     tabPanel("Plot",
-                                              br(),
-                                              radioButtons("plottype", 
-                                                           label = "Select type of plot",
-                                                           choices = list("Boxplot" = "box", 
-                                                                          "Scatterplot" = "scat",
-                                                                          "Histogram" = "hist"),
-                                                           selected = 1, inline = TRUE),
-                                              textOutput("warning"),
-                                              plotOutput("plot", width = "auto" )
-                                              ), 
-                                     tabPanel("Summary", verbatimTextOutput("summary"))
-                                     
-                                   ))
-                                  )
-                                 )),
-                    tabPanel("New panel",
-                             "This is it "),
+                                       sidebarLayout(
+                                         sidebarPanel(fileInput( "file", label = "Input your data" ),
+                                                      actionButton( "read", label = "Read table" ),
+                                                      br(),
+                                                      hr(),
+                                                      uiOutput(outputId = "factorcheckboxes"),
+                                                      width = 3),
+                                         mainPanel(
+                                           tabsetPanel(
+                                             tabPanel("Table", br(), dataTableOutput( outputId = "table")),
+                                             tabPanel("Summary", verbatimTextOutput("summary"))
+                                             
+                                           ))
+                                       )
+                             )),
+                    tabPanel("Plots",
+                             fluidPage(theme = shinytheme("united"),
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           radioButtons("plottype", 
+                                                        label = "Select type of plot",
+                                                        choices = list("Boxplot" = "box", 
+                                                                       "Scatterplot" = "scat",
+                                                                       "Histogram" = "hist"),
+                                                        selected = 1, inline = TRUE),
+                                           hr(),
+                                           br(),
+                                           selectInput(inputId = "variable.x", label = "X axis:", choices = "Please read file with your data", selected = "all"),
+                                           selectInput(inputId = "variable.y", label = "Y axis:", choices = "Please read file with your data", selected = "all"),
+                                           selectInput(inputId = "variable.color",  label = "Group by:", choices = "Please read file with your data", selected = "all"),
+                                           width = 3
+                                                      ),
+                                         mainPanel(
+                                           tabsetPanel(
+                                             tabPanel("Plot",
+                                                      br(),
+                                                      
+                                                      plotOutput("plot", width = "auto" )
+                                             ), 
+                                             tabPanel("Empty", br()),
+                                             tabPanel("Empty", br())
+                                           ))
+                                       )
+                             )),
                     navbarMenu("Another new panel",
                                tabPanel("Sub-Component A", "This is it la la la"),
                                tabPanel("Sub-Component B"))
